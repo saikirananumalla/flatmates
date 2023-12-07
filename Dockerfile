e3750b819e7edba90e0c3134ba7aa7c22e66d2ce
Varun Sai <varunsai855@gmail.com>
2023-12-07 15:25:12 -0500

    network changes

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ COPY requirements.txt ./
 
 ENV SECRET_KEY=8f934fd44042ad36975226bed085284c227a9d9ee713425fadf7d81932773932
 ENV ALGORITHM=HS256
+ENV DATABASE_HOST=host.docker.internal
 
 # Install any needed packages specified in requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
@@ -20,4 +21,5 @@ COPY . .
 EXPOSE 8000
 
 # Command to run the application
-CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000", "--reload", "--workers", "4", "--proxy-headers", "--forwarded-allow-ips", "*"]
+
--- config/db.py
+++ config/db.py
@@ -1,20 +1,22 @@
+import os
 import pymysql
 from pymysql import Connection
 from pymysql import cursors
+
+host = os.environ["DATABASE_HOST"]
 
 
 def get_connection() -> Connection:
     try:
         connection = pymysql.connect(
-            host="localhost",
+            host=host,
             port=3306,
             user="root",
-            password="password",
+            password="admin123",
             database="roommates",
             cursorclass=pymysql.cursors.Cursor,
             autocommit=True,
         )
-        cur = connection.cursor()
         return connection
     except pymysql.err.OperationalError as e:
-        print("Error: %d: %s" % (e.args[0], e.args[1]))
+        print(e)
